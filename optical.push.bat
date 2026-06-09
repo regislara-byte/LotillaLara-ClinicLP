0b94fc137ef591bc750b071e8ceeb6667c2e7b5b
Regis Lara <213967324+regislara-byte@users.noreply.github.com>
2026-06-09 20:04:33 +0200

    LL-OPTICALV2 Update

--- optical.push.bat
+++ optical.push.bat
@@ -1,6 +1,20 @@
 @echo off
 title LL-OPTICALV2 DEPLOY
+
+echo.
+echo ==========================
+echo   LL-OPTICALV2 DEPLOY
+echo ==========================
+echo.
+
 git add .
 git commit -m "LL-OPTICALV2 Update"
 git push origin main
+
+echo.
+echo ==========================
+echo   DEPLOY COMPLETE
+echo ==========================
+echo.
+
 pause
