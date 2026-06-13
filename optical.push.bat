55ecc1e2c1ab0b2a46659f3f38d3c714db54edb0
Regis Lara <213967324+regislara-byte@users.noreply.github.com>
2026-06-13 18:53:24 +0200

    LL-OPTICALV2 Update Sat 06/13/2026 18:53:24.91

--- optical.push.bat
+++ optical.push.bat
@@ -1,25 +1,39 @@
 @echo off
+title LL-OPTICALV2 DEPLOY ENGINE
 
 echo.
-echo =================================
-echo      LL-OPTICALV2 DEPLOY
-echo =================================
+echo =====================================
+echo        LL-OPTICALV2 DEPLOY
+echo =====================================
 echo.
 
 git add .
-git commit -m "LL-OPTICALV2 Update"
+
+git commit -m "LL-OPTICALV2 Update %date% %time%"
+
 git push origin main
 
 echo.
-echo =================================
-echo       DEPLOY COMPLETE
-echo =================================
+echo =====================================
+echo          PUSH COMPLETE
+echo =====================================
 echo.
 
-start https://github.com/regislara-byte.github.io/LL-Optical-v2/
+echo Waiting for GitHub Pages update...
+timeout /t 5 >nul
 
 echo.
-echo Opening GitHub Repository...
+echo Opening Repository...
+start https://github.com/regislara-byte/LotillaLara-ClinicLP
+
+echo.
+echo Opening Live Website...
+start https://regislara-byte.github.io/LotillaLara-ClinicLP/
+
+echo.
+echo =====================================
+echo       DEPLOYMENT FINISHED
+echo =====================================
 echo.
 
 pause
